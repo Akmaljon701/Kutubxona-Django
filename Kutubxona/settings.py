bde3cafc786c2ef980e905565699b0d2f103d560
Akmaljon <akmalkhanever@gmail.com>
2023-02-11 09:50:37 +0500

    Debug True ga o'zgartirildi

--- Kutubxona/settings.py
+++ Kutubxona/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^0dzd8-+oeh$!n_ivq*mqv6k70=zdpcy3y^)ztb8%p-n+p$$fs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
